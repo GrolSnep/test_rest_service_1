51c2e49d6b8222eea5a42ffb364c89401dec9c11
Dima <dima.tuhbatullin@mai.ru>
2023-03-23 22:48:57 +0500

    123

---
+++ src/main/java/ru/students/test_rest_service_1/service/MyModifyService.java
@@ -0,0 +1,8 @@
+package ru.students.test_rest_service_1.service;
+
+import ru.students.test_rest_service_1.model.Response;
+
+public interface MyModifyService
+{
+    Response modify (Response response);
+}
